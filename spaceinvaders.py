998ab31d027b1c6053950340fe301f8e800694c3
moRhen <morhenmail@gmail.com>
2015-10-14 13:56:45 +0200

    starting all over

--- spaceinvaders.py
+++ spaceinvaders.py
@@ -1,42 +1,16 @@
 #!/usr/bin/env python3
 import time
 import curses
-import os
+
 #print('\x1b[8;20;36t')
 
-class spaceinvaders(object):
-
-    def __init__(self):
-        pass
-
-    def termsize(self):
-        rows, columns = os.popen('stty size', 'r', 1).read().split()
-        return rows, columns
-
-    def aliens(self):
-    #generate 5x11 alien matrix / 3 types of aliens
-        #alien = (('^' * 11+'\n')+(('¤' * 11+'\n')*2)+(('ж' * 11+'\n')*2))
-        alien = []
-        alien.append('^' * 11)#30pkt
-        alien.append('¤' * 11)#20pkt
-        alien.append(alien[1])
-        alien.append('ж' * 11)#10pkt
-        alien.append(alien[3])
-        return alien
-
-    def defence(self):
-
-        defence = []
-        defence.append(' '+(('#'+4*' ')*3)+'#'+' ')
-        defence.append(((3*'#'+2*' ')*3)+3*'#')
-        return defence
-
-class Ship(object):
+class Spaceinvaders(object):
 
     def __init__(self, stdscr):
+        self.stdscr = stdscr
         self.pos_y = 17
         self.pos_x = 18
-        self.pos_x_last = 18
+        self.pos_x_last = 0
         self.min_x = 0
         self.max_x = 35
         self.lives = [3, '⍊', '⍊', '⍊']
@@ -44,10 +18,21 @@ class Ship(object):
         self.pos_x_shoot = 0
         self.shoot_time = 0
         self.shoots = 0
-        self.score = '0000'
-        self.stdscr = stdscr
+        self.score = 0
+        self.defence = [(14, 10), (14, 15), (14, 20), (14, 25), (15, 9), (15, 10), (15, 11), (15, 14), (15, 15), (15, 16), (15, 19), (15, 20), (15, 21), (15, 24), (15, 25), (15, 26)]
 
-    def move(self, event):
+    def reset(self):
+        self.pos_x = 18
+        self.pos_x_last = 0
+        self.pos_x_shoot = 0
+        self.shoot_time = 0
+        self.shoots = 0
+        self.lives = [3, '⍊', '⍊', '⍊']
+        self.score = 0
+        self.defence = [(14, 10), (14, 15), (14, 20), (14, 25), (15, 9), (15, 10), (15, 11), (15, 14), (15, 15), (15, 16), (15, 19), (15, 20), (15, 21), (15, 24), (15, 25), (15, 26)]
+
+    def ship(self, event):
+        #move#######
         if event == curses.KEY_LEFT:
             self.pos_x_last = self.pos_x
             self.pos_x -= 1
@@ -58,63 +43,76 @@ class Ship(object):
             self.pos_x += 1
             if self.pos_x > 34:
                 self.pos_x = self.max_x
+        #shoot######
+        if event == 32:
+            self.shoots = 1
+            self.pos_x_shoot = self.pos_x
 
-    def shoot(self, event):
-        self.shoots = 1
-        self.pos_x_shoot = self.pos_x
+    def aliens(self):
+        pass
 
     def render(self):
-        if self.pos_x_last != self.pos_x:
-            self.stdscr.addstr(self.pos_y, self.pos_x_last, ' ')
+        #upperbar#######
+        self.stdscr.addstr(0, 0, 'SCORE')
+        self.stdscr.addstr(1, 0, '{:0>5}'.format(self.score))
+        #bottombar######
+        self.stdscr.addstr(18, 0, '_' * 36)
+        self.stdscr.addstr(19, 0, '{}  {}'.format(self.lives[0], ' '.join(self.lives[1:])))
+        #ship###########
         self.stdscr.addstr(self.pos_y, self.pos_x, '⍊')
-        if self.pos_y_shoot == 0:
+        if self.pos_x != self.pos_x_last:
+            self.stdscr.addstr(self.pos_y, self.pos_x_last, ' ')
+        #ship#shoot#####
+        if self.pos_y_shoot == 1:
             self.shoots = 0
             self.stdscr.addstr(self.pos_y_shoot + 1, self.pos_x_shoot, ' ')
             self.pos_y_shoot = 16
-        if self.shoots != 0 and self.pos_y_shoot > 0:
+        if self.shoots != 0 and self.pos_y_shoot > 1:
             if self.pos_y_shoot < 16:
                 self.stdscr.addstr(self.pos_y_shoot + 1, self.pos_x_shoot, ' ')
             self.stdscr.addstr(self.pos_y_shoot, self.pos_x_shoot, '|')
-            if time.time() - self.shoot_time > 0.1:
+            if time.time() - self.shoot_time > 0.15:
                 self.pos_y_shoot -= 1
                 self.shoot_time = time.time()
+        #ship#shoot#hit#
+        shotposchar = str(self.stdscr.inch(self.pos_y_shoot, self.pos_x_shoot))
+        if shotposchar != '32' and shotposchar != '124':
+            self.stdscr.addstr(self.pos_y_shoot, self.pos_x_shoot, ' ')#shoting pos
+            self.stdscr.addstr(self.pos_y_shoot + 1, self.pos_x_shoot, ' ')#clear shooting animation
+            if (self.pos_y_shoot, self.pos_x_shoot) in self.defence:
+                self.defence.remove((self.pos_y_shoot, self.pos_x_shoot))
+                self.score = self.score + 20#for test only
+                self.shoots = 0
+                self.pos_y_shoot = 16
+        #defence########
+        for n in self.defence:
+            self.stdscr.addstr(n[0], n[1], '#')
+        #TESTOWA########
+        a = str(self.stdscr.inch(self.pos_y_shoot, self.pos_x_shoot))
+        self.stdscr.addstr(2, 0, a)
 
-
-#MAIN FUNCTION
 def main(stdscr):
-    ship = Ship(stdscr)
+    #additional curses settings
+    curses.curs_set(0)
+    stdscr.nodelay(1)
+    ###########################
+    spaceinv = Spaceinvaders(stdscr)
     while True:
-        curses.curs_set(0)
-        stdscr.addstr(0, 0, 'SCORE')
-        stdscr.addstr(1, 0, ship.score)
-        stdscr.addstr(18, 0, ('_' * 36))
-        stdscr.addstr(19, 0, ('{}  {}'.format(ship.lives[0], ' '.join(ship.lives[1:]))))
-        ship.render()
+        spaceinv.render()
         event = stdscr.getch()
         if event in [curses.KEY_LEFT, curses.KEY_RIGHT]:
-            ship.move(event)
-        if event == 32:
-            if ship.shoots == 0:
-                ship.shoot(event)
-                ship.shoot_time = time.time()
+            spaceinv.ship(event)
+        elif event == 32:
+            if spaceinv.shoots == 0:
+                spaceinv.ship(event)
+                spaceinv.shoot_time = time.time()
+        elif event == ord('p'):
+            stdscr.clear()
+            spaceinv.reset()
         elif event == ord('q'):
-            print('Przegrałeś')
             break
-        stdscr.nodelay(1)
+        
 
-#s = spaceinvaders()
-#s.screen()
+
 if __name__ == "__main__":
      curses.wrapper(main)
-
-#if __name__ == "__main__":
-#    spaceinvaders().screen()
-
-
-
-
-# Remove cursor and input echo.
-#curses.curs_set(0)
-#curses.noecho()
-
-# ±, ж, ¤, Ѧ, ѧ, ѫ, ⍎, ⍊, ┻, ╨
